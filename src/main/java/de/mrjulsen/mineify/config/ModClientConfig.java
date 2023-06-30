dc0d86088fc18c50fd89d87d2effa03ed237a61f
MisterJulsen <geisterdrache02@gmail.com>
2023-06-30 15:39:07 +0200

    Added config

---
+++ src/main/java/de/mrjulsen/mineify/config/ModClientConfig.java
@@ -0,0 +1,35 @@
+package de.mrjulsen.mineify.config;
+
+import de.mrjulsen.mineify.client.EUserSoundVisibility;
+import de.mrjulsen.mineify.sound.AudioFileConfig;
+import de.mrjulsen.mineify.sound.ESoundChannels;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
+
+public class ModClientConfig {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+
+    
+    public static final ForgeConfigSpec.ConfigValue<ESoundChannels> DEFAULT_CHANNELS;
+    public static final ForgeConfigSpec.ConfigValue<EUserSoundVisibility> DEFAULT_VISIBILITY;
+    public static final ConfigValue<Integer> DEFAULT_QUALITY;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> ACTIVATION;
+
+    static {
+        BUILDER.push("Mineify Client Config");
+
+        /* CONFIGS */
+        DEFAULT_VISIBILITY = BUILDER.comment("Preselected visibility when uploading a sound.")
+                .defineEnum("defaults.sound_visibility", EUserSoundVisibility.PRIVATE);
+        DEFAULT_CHANNELS = BUILDER.comment("Preselected channels when uploading a sound.")
+                .defineEnum("defaults.sound_channels", ESoundChannels.MONO);
+        DEFAULT_QUALITY = BUILDER.comment("Preselected quality when uploading a sound.")
+                .defineInRange("defaults.sound_quality", 5, AudioFileConfig.OGG_QUALITY_MAX, AudioFileConfig.OGG_QUALITY_MIN);                
+        ACTIVATION = BUILDER.comment("When false, you won't hear any sounds played by this mod.")
+                .define("enabled", true);
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
