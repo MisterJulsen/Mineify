e4ca029bd4c9cbd604fa68614d009f122fa4bc04
MisterJulsen <julian.ramsperger@t-online.de>
2023-07-11 14:02:16 +0200

    Added config for size parameters

--- src/main/java/de/mrjulsen/mineify/config/ModCommonConfig.java
+++ src/main/java/de/mrjulsen/mineify/config/ModCommonConfig.java
@@ -10,7 +10,10 @@ public class ModCommonConfig {
     public static final ForgeConfigSpec.ConfigValue<Integer> MAX_STORAGE_SPACE_BYTES;
     public static final ForgeConfigSpec.ConfigValue<Integer> MAX_FILES;
     public static final ForgeConfigSpec.ConfigValue<Integer> MAX_FILENAME_LENGTH;
-    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_AUDIO_DURATION;
+    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_AUDIO_DURATION;    
+    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_RADIUS;
+    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_VOLUME;
+    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_BOX_SIZE;
     public static final ForgeConfigSpec.ConfigValue<Integer> EXPERIMENTAL_SLOW_STREAMING;
     public static final ForgeConfigSpec.BooleanValue EXPERIMENTAL_STREAM_REQUEST;
 
@@ -18,17 +21,27 @@ public class ModCommonConfig {
         BUILDER.push("mineify_common_config");
 
         
-        MAX_FILE_SIZE_BYTES = BUILDER.comment("Maximum size per file in bytes. < 0: unlimited (Default: 200 MB)")
+        MAX_FILE_SIZE_BYTES = BUILDER.comment("Maximum size per file in bytes. < 0: unlimited (Default: 204800000 [200 MB])")
                 .define("files.max_size", 204800000);
-        MAX_FILES = BUILDER.comment("Maximum count of files a user can upload. < 0: unlimited")
+        MAX_FILES = BUILDER.comment("Maximum count of files a user can upload. < 0: unlimited (Default: -1)")
                 .define("files.max_count", -1);
-        MAX_STORAGE_SPACE_BYTES = BUILDER.comment("Maximum storage size in bytes per user for all uploads. < 0: unlimited (Default: 1 GB)")
+        MAX_STORAGE_SPACE_BYTES = BUILDER.comment("Maximum storage size in bytes per user for all uploads. < 0: unlimited (Default: -1)")
                 .define("files.max_storage", -1);
                 
         MAX_FILENAME_LENGTH = BUILDER.comment("Max filename length.")
                 .defineInRange("files.max_filename_length", 32, 10, 100);
         MAX_AUDIO_DURATION = BUILDER.comment("Maximum duration of sounds in seconds. < 0: unlimited")
                 .define("files.max_duration", -1);                
+        
+
+        MAX_RADIUS = BUILDER.comment("Max possible radius.")
+                .defineInRange("range.max_radius", 256, 1, 1024);                
+        MAX_BOX_SIZE = BUILDER.comment("Max possible box size.")
+                .defineInRange("range.max_box_size", 256, 1, 1024);                
+        MAX_VOLUME = BUILDER.comment("Max possible volume.")
+                .defineInRange("range.max_volume", 64, 0, 256);                
+        
+
         EXPERIMENTAL_SLOW_STREAMING = BUILDER.comment("EXPERIMENTAL! When not 0, sound data is being streamed slower to clients. The defined value is the delay in milliseconds. May improve client and server performance. This option will not take effect when stream_request_enabled = true")
                 .defineInRange("experimental.slow_streaming", 0, 0, 1000);
 
